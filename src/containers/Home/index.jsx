ba159b080539a5bb4070b40dbd3434990ebf8c5f
PedroNogueir4 <pedroffn209@gmail.com>
2023-06-27 19:24:10 -0300

    Consumida api de filmes e página de Home iniciada

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,7 +1,42 @@
+import { useEffect, useState } from 'react'
+
+import api from '../../services/api'
+import { Background, Info, Card, Container } from './styles'
+
 export function Home() {
+  const [movie, setMovie] = useState()
+
+  useEffect(() => {
+    async function loadMovies() {
+      const {
+        data: { results }
+      } = await api.get('/movie/popular')
+      setMovie(results[3])
+    }
+
+    loadMovies()
+  }, [])
+
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <>
+      {movie && (
+        <Background
+          img={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
+        >
+          <Container>
+            <Info>
+              <h1>{movie.title}</h1>
+              <p>{movie.overview}</p>
+            </Info>
+            <Card>
+              <img
+                alt="capa-do-filme"
+                src={`https://image.tmdb.org/t/p/w200/${movie.poster_path}`}
+              />
+            </Card>
+          </Container>
+        </Background>
+      )}
+    </>
   )
 }
---
+++ src/containers/Home/styles.js
@@ -0,0 +1,48 @@
+import { styled } from 'styled-components'
+
+export const Background = styled.div`
+  background-image: url(${(props) => props.img});
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &::before {
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+`
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  max-width: 1100px;
+`
+export const Info = styled.div`
+  z-index: 2;
+  color: #ffffff;
+  width: 50%;
+
+  h1 {
+    font-weight: 700;
+    font-size: 4rem;
+  }
+  p {
+    font-weight: 500;
+    font-size: 20px;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+`
+export const Card = styled.div`
+  z-index: 2;
+  img {
+    border-radius: 30px;
+    width: 230px;
+  }
+`
