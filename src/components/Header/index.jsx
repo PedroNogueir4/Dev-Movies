d98298bd602296ada06025738476db6a943627df
PedroNogueir4 <pedroffn209@gmail.com>
2023-06-28 13:37:57 -0300

    Navegação entre páginas realizada

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,26 @@
+import { useLocation } from 'react-router-dom'
+
 import Logo from '../../assets/logo.png'
+import { Container, ButtonNav } from './styles'
 
 function Header() {
-  return <img src={Logo} style={{ width: 300 }} />
+  const location = useLocation()
+  return (
+    <Container>
+      <img src={Logo} style={{ width: 300 }} />
+      <div>
+        <ButtonNav to="/" isActive={location.pathname === '/'}>
+          Home
+        </ButtonNav>
+        <ButtonNav to="/series" isActive={location.pathname === '/series'}>
+          Séries
+        </ButtonNav>
+        <ButtonNav to="/movies" isActive={location.pathname === '/movies'}>
+          Filmes
+        </ButtonNav>
+      </div>
+    </Container>
+  )
 }
 
 export default Header
---
+++ src/components/Header/styles.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom'
+
+import { styled } from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 5px;
+  padding-right: 50px;
+  div {
+    display: flex;
+    gap: 40px;
+    align-items: center;
+  }
+`
+export const ButtonNav = styled(Link)`
+  color: white;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-weight: 700;
+  font-size: 17px;
+
+  padding-bottom: 5px;
+  border-bottom: ${(props) => (props.isActive ? '2.5px solid red' : 'none')};
+`
