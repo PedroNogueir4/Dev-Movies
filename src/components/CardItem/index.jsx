e7f0e725ac32bd8291d52c0105201fe22e05453e
PedroNogueir4 <pedroffn209@gmail.com>
2023-07-04 11:35:12 -0300

    Função de navegação ao clicar nos filmes implementada

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,12 +1,23 @@
+import { useNavigate } from 'react-router-dom'
+
 import { getImage } from '../../utils/getImages'
 import { Container, Image } from './styles'
 
 function CardItem({ item, detailWidth }) {
+  const navigate = useNavigate()
+
+  function navigateDetail() {
+    if (item.poster_path && !item.first_air_date) {
+      // (!item.first_air_date) = Bloqueio por conta de erro na API de terceiros
+      navigate(`/detalhes/${item.id}`)
+    }
+  }
   return (
     <>
       {(item.profile_path || item.poster_path) && (
         <Container>
           <Image
+            onClick={navigateDetail}
             detailWidth={detailWidth}
             src={getImage(item.poster_path || item.profile_path)}
           />
