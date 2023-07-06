de3d5de28be339ea9d85d3604f238eddfdebef8d
PedroNogueir4 <pedroffn209@gmail.com>
2023-07-06 12:48:47 -0300

    Página de detalhes responsiva

--- src/containers/Detail/styles.js
+++ src/containers/Detail/styles.js
@@ -13,6 +13,10 @@ export const Container = styled.div`
   color: white;
   font-weight: 700;
   padding-bottom: 15px;
+
+  @media (max-width: 780px) {
+    overflow: hidden;
+  }
 `
 export const Background = styled.div`
   background-image: url(${(props) => props.img});
@@ -46,6 +50,12 @@ export const ContainerMain = styled.div`
   justify-content: center;
   align-items: flex-start;
   margin-top: -100px;
+
+  @media (max-width: 780px) {
+    flex-direction: column;
+    align-items: center;
+    margin-top: -155px;
+  }
 `
 export const ContainerInfo = styled.div`
   display: flex;
@@ -56,6 +66,10 @@ export const ContainerInfo = styled.div`
   z-index: 2;
   h1 {
     font-size: 50px;
+
+    @media (max-width: 460px) {
+      font-size: 38px;
+    }
   }
 `
 export const Genres = styled.div`
@@ -63,10 +77,27 @@ export const Genres = styled.div`
   gap: 15px;
   align-items: center;
 
+  @media (max-width: 780px) {
+    justify-content: center;
+  }
+  @media (max-width: 460px) {
+    width: 89%;
+  }
+
   p {
     border: 2px solid white;
     border-radius: 30px;
     padding: 7px 18px;
+
+    @media (max-width: 900px) {
+      font-size: 14px;
+      padding: 6px 13px;
+    }
+
+    @media (max-width: 460px) {
+      font-size: 13.5px;
+      padding: 4px 10px;
+    }
   }
 `
 export const CardMovie = styled.div`
@@ -75,6 +106,9 @@ export const CardMovie = styled.div`
     border-radius: 30px;
     width: 350px;
     animation: ${scale} 0.5s linear;
+    @media (max-width: 780px) {
+      width: 250px;
+    }
   }
 `
 export const ContainerVideos = styled.div`
@@ -90,8 +124,16 @@ export const ContainerVideos = styled.div`
     flex-direction: column;
     align-items: center;
     gap: 15px;
+
+    @media (max-width: 780px) {
+      font-size: 13px;
+      padding: 0px 11px;
+    }
   }
   iframe {
     border: none;
+    @media (max-width: 460px) {
+      width: 80%;
+    }
   }
 `
